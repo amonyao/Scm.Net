d62565807d6d6baa766aed319d61794a05a198d7
opemscm <361341288@qq.com>
2025-06-26 10:50:04 +0800

    服务端项目依赖调整。

--- net/Samples.Server/Readme.txt
+++ net/Samples.Server/Readme.txt
@@ -7,8 +7,11 @@
 【项目依赖】
 1、Scm.Common：框架提供的一些公共基础方法；
 2、Scm.Common.Dto：Dto基类相关；
-3、Scm.Server：框架提供的服务相关方法；
-4、Scm.Server.Dao：Dao基类相关；
-5、Scm.Server.Api：框架提供的动态接口类库，用于提供Service的接口转换；
-6、Scm.Dsa.Dba.Sugar：数据库切面服务；
-7、SqlSuagerCore：数据库ORM框架。
+3、Scm.Common.Text：字符相关工具；
+4、Scm.Dao：Dao基类相关；
+5、Scm.Dsa.Dba.Sugar：数据库切面服务；
+6、Scm.Server：框架提供的服务相关方法；
+7、Scm.Server.Api：框架提供的动态接口类库，用于提供Service的接口转换；
+8、Scm.Server.Dao：服务端Dao基类相关；
+9、SqlSuagerCore：数据库ORM框架；
+10、MiniExcel：Excel文件快速导入导出辅助类。
--- net/Scm.Api/Controllers/OnLineController.cs
+++ net/Scm.Api/Controllers/OnLineController.cs
@@ -5,30 +5,31 @@ using Com.Scm.Utils;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
-namespace Com.Scm.Api.Controllers;
-
-/// <summary>
-/// 在线用户
-/// </summary>
-[ApiExplorerSettings(GroupName = "v1")]
-public class OnLineController : ApiController
+namespace Com.Scm.Api.Controllers
 {
-    private readonly ICacheService _cacheService;
-
-    public OnLineController(ICacheService cacheService)
-    {
-        _cacheService = cacheService;
-    }
-
     /// <summary>
-    /// 获取所有在线信息
+    /// 在线用户
     /// </summary>
-    /// <returns></returns>
-    [HttpGet, AllowAnonymous]
-    public List<ClientUser> Get()
+    [ApiExplorerSettings(GroupName = "Scm")]
+    public class OnLineController : ApiController
     {
-        var userList = _cacheService.GetCache<List<ClientUser>>(KeyUtils.ONLINEUSERS);
+        private readonly ICacheService _cacheService;
 
-        return userList ?? new List<ClientUser>();
+        public OnLineController(ICacheService cacheService)
+        {
+            _cacheService = cacheService;
+        }
+
+        /// <summary>
+        /// 获取所有在线信息
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet, AllowAnonymous]
+        public List<ClientUser> Get()
+        {
+            var userList = _cacheService.GetCache<List<ClientUser>>(KeyUtils.ONLINEUSERS);
+
+            return userList ?? new List<ClientUser>();
+        }
     }
 }
